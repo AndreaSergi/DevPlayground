d476a5142b54ad6a4601895c6ffdd81f47c99558
martinamancuso <martinamancuso24@gmail.com>
2024-03-15 14:58:53 +0100

    react-router-dom installed; tris folder, App.jsx and main.jsx created and updated

--- devplayground/src/main.jsx
+++ devplayground/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.jsx";
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
---
+++ devplayground/src/tris/Tris.jsx
@@ -0,0 +1,3 @@
+export function Tris() {
+  return <p>Hello, Tris!</p>;
+}
