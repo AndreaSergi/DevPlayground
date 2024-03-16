de12f7723010004f2ed6e6db9aac189455ef8668
martinamancuso <martinamancuso24@gmail.com>
2024-03-16 10:27:05 +0100

    layout Tris created

--- devplayground/src/tris/Tris.jsx
+++ devplayground/src/tris/Tris.jsx
@@ -1,14 +1,19 @@
 import { TrisLogo } from "./TrisLogo";
 import { TrisButton } from "./TrisButton";
+import style from "./Tris.module.css";
 
 export function Tris() {
   return (
-    <div>
-      <TrisLogo />
-      <TrisButton>3x3</TrisButton>
-      <TrisButton>Player vs Player</TrisButton>
-      <TrisButton>Rules</TrisButton>
-      <TrisButton>Dark/Light</TrisButton>
-    </div>
+      <div className={style.page}>
+        <div className={style.container}>
+          <TrisLogo />
+          <TrisButton>3x3</TrisButton>
+          <TrisButton>Player vs Player</TrisButton>
+          <TrisButton>Rules</TrisButton>
+        </div>
+        <div className={style.themeButton}>
+          <TrisButton>Dark/Light</TrisButton>
+        </div>
+      </div>
   );
 }
