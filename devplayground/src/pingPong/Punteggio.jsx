be8f42e3cb445bc172882a3c01e5c08ae1d8e729
Davide Lodde <johndoe@example.com>
2024-03-17 19:37:33 +0100

    aggiunto background punteggio

--- devplayground/src/pingPong/Punteggio.jsx
+++ devplayground/src/pingPong/Punteggio.jsx
@@ -4,12 +4,12 @@ export function Punteggio({ player, opponent }) {
     padding: '10px',
     margin: '10px 0',
     borderRadius: '5px',
-    background: 'linear-gradient(to right, #1e3c72, #2a5298)',
+    background: 'url("backgroundPunteggio.png")',
     color: '#ffd702',
     textAlign: 'center',
     boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
     fontFamily: '"Lucida Console", Monaco, monospace',
-    border: '5px solid #dbb906' // Aggiunto bordo color oro
+    border: '5px solid #dbb906'
   };
 
   return (
