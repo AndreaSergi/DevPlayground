16de1b5ad5ef98e8e19a4c3e1d99c83292e33a8d
martinamancuso <martinamancuso24@gmail.com>
2024-03-17 12:08:05 +0100

    Menu moved inside TrisMenu

--- devplayground/src/App.jsx
+++ devplayground/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { Tris } from "./tris/Tris";
+import { TrisMenu } from "./tris/TrisMenu/TrisMenu";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Link to="/tris">Tris</Link>} />
-      <Route path="/tris" element={<Tris />} />
-      <Route path="/tris/game" element={<p>game</p>} />
+      <Route path="tris" element={<Tris />}>
+        <Route index element={<TrisMenu />} />
+        <Route path="game" element={<p>game</p>} />
+      </Route>
     </Routes>
   );
 }
