f62dd7345a6cc2dda872a53975e87c9d213dbd97
martinamancuso <martinamancuso24@gmail.com>
2024-03-25 15:19:08 +0100

    fontoshan handwrite added to trix.css; tris -> trix updated in TrisLogo.jsx

--- devplayground/src/tris/Trix.jsx
+++ devplayground/src/tris/Trix.jsx
@@ -1,80 +1,119 @@
-import React, { useEffect, useState } from 'react';
-import './Trix.css';
+import React, { useEffect, useState } from "react";
+import "./Trix.css";
+import style from "../tris/TrisButton/TrisButton.module.css";
 
 const BOARD_SIZE = 3;
 
 export function Trix() {
-  const [board, setBoard] = useState(Array.from({ length: BOARD_SIZE }, () => Array.from({ length: BOARD_SIZE }, () => null)));
-  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [board, setBoard] = useState(
+    Array.from({ length: BOARD_SIZE }, () =>
+      Array.from({ length: BOARD_SIZE }, () => null)
+    )
+  );
+  const [currentPlayer, setCurrentPlayer] = useState("X");
   const [winner, setWinner] = useState(null);
   const [winningCells, setWinningCells] = useState([]);
   const [scoreX, setScoreX] = useState(0);
   const [scoreO, setScoreO] = useState(0);
 
-
   function checkWinner() {
     // Check rows
     for (let i = 0; i < BOARD_SIZE; i++) {
-      if (board[i][0] && board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+      if (
+        board[i][0] &&
+        board[i][0] === board[i][1] &&
+        board[i][0] === board[i][2]
+      ) {
         setWinner(board[i][0]);
 
-        setWinningCells([[i, 0], [i, 1], [i, 2]]);
+        setWinningCells([
+          [i, 0],
+          [i, 1],
+          [i, 2],
+        ]);
         return;
       }
     }
 
     // Check columns
     for (let i = 0; i < BOARD_SIZE; i++) {
-      if (board[0][i] && board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+      if (
+        board[0][i] &&
+        board[0][i] === board[1][i] &&
+        board[0][i] === board[2][i]
+      ) {
         setWinner(board[0][i]);
 
-        setWinningCells([[0, i], [1, i], [2, i]]);
+        setWinningCells([
+          [0, i],
+          [1, i],
+          [2, i],
+        ]);
         return;
       }
     }
 
     // Check diagonals
-    if (board[0][0] && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+    if (
+      board[0][0] &&
+      board[0][0] === board[1][1] &&
+      board[0][0] === board[2][2]
+    ) {
       setWinner(board[0][0]);
 
-      setWinningCells([[0, 0], [1, 1], [2, 2]]);
+      setWinningCells([
+        [0, 0],
+        [1, 1],
+        [2, 2],
+      ]);
       return;
     }
 
-    if (board[0][2] && board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+    if (
+      board[0][2] &&
+      board[0][2] === board[1][1] &&
+      board[0][2] === board[2][0]
+    ) {
       setWinner(board[0][2]);
 
-      setWinningCells([[0, 2], [1, 1], [2, 0]]);
+      setWinningCells([
+        [0, 2],
+        [1, 1],
+        [2, 0],
+      ]);
       return;
     }
 
     // Check for draw
     if (!board.flat().includes(null)) {
-      setWinner('Draw');
+      setWinner("Draw");
     }
-
-
-  };
+  }
 
   function handleCellClick(row, col) {
     if (!board[row][col] && !winner) {
       const newBoard = [...board];
       newBoard[row][col] = currentPlayer;
       setBoard(newBoard);
-      setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+      setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
       checkWinner();
-
     }
-  };
+  }
 
   function renderCell(row, col) {
-    const isWinningCell = winningCells.some(cell => cell[0] === row && cell[1] === col);
+    const isWinningCell = winningCells.some(
+      (cell) => cell[0] === row && cell[1] === col
+    );
     return (
-      <div className={`cell ${isWinningCell ? 'winning-cell' : ''}`} key={`${row}-${col}`} onClick={() => handleCellClick(row, col)}>
+      <div
+        className={`cell ${isWinningCell ? "winning-cell" : ""}`}
+        key={`${row}-${col}`}
+        onClick={() => handleCellClick(row, col)}
+      >
         {board[row][col]}
       </div>
     );
-  };
+  }
 
   function renderBoard() {
     return board.map((row, rowIndex) => (
@@ -82,23 +121,26 @@ export function Trix() {
         {row.map((cell, colIndex) => renderCell(rowIndex, colIndex))}
       </div>
     ));
-  };
+  }
 
   const initializeBoard = () => {
-    setBoard(Array.from({ length: BOARD_SIZE }, () => Array.from({ length: BOARD_SIZE }, () => null)));
-    setCurrentPlayer('X');
+    setBoard(
+      Array.from({ length: BOARD_SIZE }, () =>
+        Array.from({ length: BOARD_SIZE }, () => null)
+      )
+    );
+    setCurrentPlayer("X");
     setWinner(null);
     setWinningCells([]);
   };
 
   function checkWinnerAndAssignScore() {
     // Logica per controllare il vincitore rimane inalterata
-    if (winner === 'X') {
+    if (winner === "X") {
       setScoreX(scoreX + 1); // Assegna un punto a X
-    } else if (winner === 'O') {
+    } else if (winner === "O") {
       setScoreO(scoreO + 1); // Assegna un punto a O
     }
-
   }
 
   // function resetScore() {
@@ -108,23 +150,24 @@ export function Trix() {
   // }
 
   useEffect(() => {
-    checkWinnerAndAssignScore()
-  }, [winner])
-
+    checkWinnerAndAssignScore();
+  }, [winner]);
 
   return (
     <div className="trix">
       <h1>Trix</h1>
 
       {!winner && <p>Current player: {currentPlayer}</p>}
-      {winner && <p>{winner === 'Draw' ? 'It\'s a draw!' : `Player ${winner} wins!`}</p>}
-      <button onClick={initializeBoard}>Rivincita</button>
+      {winner && (
+        <p>{winner === "Draw" ? "It's a draw!" : `Player ${winner} wins!`}</p>
+      )}
+      <button className={style.button} onClick={initializeBoard}>
+        Rivincita
+      </button>
 
       <div className="board">
-        <div className='layer'> {renderBoard()}</div>
+        <div className="layer"> {renderBoard()}</div>
       </div>
-
-      
 
       <div className="scoreboard">
         <h2>Punteggio</h2>
@@ -136,4 +179,4 @@ export function Trix() {
       </div>
     </div>
   );
-};
+}
